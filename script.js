bbccc0fdfcf816066a926f7c9a3283bf499290c9
Prashant Pal <palprashant465@gmail.com>
2025-03-13 22:09:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function firstWord(s) {
-  // your code here
+    // Trim leading and trailing spaces
+    s = s.trim();
+    
+    // Find index of first space
+    const index = s.indexOf(' ');
+    
+    // If no space is found, return the entire string
+    if (index === -1) return s;
+    
+    // Return substring from 0 to first space
+    return s.substring(0, index);
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
